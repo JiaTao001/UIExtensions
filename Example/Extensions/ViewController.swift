623cf20ebdc3926f1690a951d220f980bfc4566c
Faria01 <2233945666@qq.com>
2021-12-03 14:44:03 +0800

    Initial commit

---
+++ Example/Extensions/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Extensions
+//
+//  Created by Faria01 on 12/03/2021.
+//  Copyright (c) 2021 Faria01. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
